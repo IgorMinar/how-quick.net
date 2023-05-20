98426297cbf6658bb1275e5885b6f2638d97de9c
Igor Minar <i@igor.dev>
2023-05-20 11:32:14 -0700

    fix(html): update document metadata

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -16,11 +16,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "how-quick.net",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "How quick is your Web?",
     },
   ],
 };
