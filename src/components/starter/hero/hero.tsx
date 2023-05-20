65daba832b14904a82ac96af35cef16ebd9659c6
Igor Minar <i@igor.dev>
2023-05-20 11:21:33 -0700

    fix: improve wording + measururing technique

--- src/components/starter/hero/hero.tsx
+++ src/components/starter/hero/hero.tsx
@@ -7,9 +7,15 @@ export default component$(() => {
       <h1>
         How <span class="highlight">quick</span>
         <br />
-        is your <span class="highlight">net</span>?
+        is your <span class="highlight">Web</span>?
       </h1>
-      <p>We used a full-stack application to measured the end-to-end latency of your Web experience...</p>
+      <p>
+        We used a full-stack application to measured the{" "}
+        <a href="https://developer.mozilla.org/en-US/docs/Web/Performance/Navigation_and_resource_timings#duration:~:text=Copy%20to%20Clipboard-,Duration,-We%20are%20provided">
+          end-to-end latency
+        </a>{" "}
+        of your Web experience...
+      </p>
     </div>
   );
 });
--- src/components/starter/timer/timer.tsx
+++ src/components/starter/timer/timer.tsx
@@ -1,25 +1,32 @@
 import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import styles from "./timer.module.css";
 import Gauge from "../gauge";
-import { useStore } from "@builder.io/qwik";
+//import { useStore } from "@builder.io/qwik";
 
 export default component$(() => {
   const duration = useSignal(0);
-  const rawPerfResults = useStore<Array<Omit<PerformanceEntry, "toJSON">>>([]);
+  //const rawPerfResults = useStore<Array<Omit<PerformanceEntry, "toJSON">>>([]);
   //const colo = ${request.cf.colo};
 
   useVisibleTask$(
     () => {
-      const observer = new PerformanceObserver((list) => {
-        list.getEntries().forEach((entry) => {
-          rawPerfResults.push(entry);
-          duration.value = entry.duration;
-        });
-      });
+      // this doesn't work for some reason
+      // "nagivation" event is never delivered, only "resource" events
+      // const observer = new PerformanceObserver((list) => {
+      //   list.getEntries().forEach((entry) => {
+      //     console.log(entry.entryType, entry.toJSON());
+      //     rawPerfResults.push(entry);
+      //     //if (entry.entryType === "navigation") {
+      //     if (!duration.value) {
+      //       duration.value = entry.duration;
+      //     }
+      //   });
+      // });
+      // observer.observe({ entryTypes: ["navigation", "resource"] }); // why doesn't 'navigation' work here?
 
-      observer.observe({ entryTypes: ["navigation"] });
+      duration.value = performance.getEntriesByType("navigation")[0].duration;
     },
-    { strategy: "document-ready" }
+    { strategy: "document-idle" }
   );
 
   return (
